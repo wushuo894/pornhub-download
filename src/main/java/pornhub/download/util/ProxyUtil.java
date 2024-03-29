8ccc92089cc1c4274c6a5c1714b3dbbb739e13a3
wushuo <wushuo894@outlook.com>
2024-03-29 22:54:36 +0800

    修改超时时间

--- src/main/java/pornhub/download/util/ProxyUtil.java
+++ src/main/java/pornhub/download/util/ProxyUtil.java
@@ -5,7 +5,9 @@ import pornhub.download.Main;
 import pornhub.download.entity.Config;
 
 public class ProxyUtil {
-    public static HttpRequest addProxy(HttpRequest request){
+    public static HttpRequest addProxy(HttpRequest request) {
+        request.setConnectionTimeout(3000)
+                .timeout(3000);
         Config config = Main.config;
         Config.Proxy proxy = config.getProxy();
         if (proxy.hasProxy()) {
