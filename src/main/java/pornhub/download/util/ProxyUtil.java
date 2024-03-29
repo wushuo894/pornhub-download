1e551a6ec7eda53216355a763ca257650752cef1
wushuo <2725730722@qq.com>
2024-03-29 22:57:03 +0800

    修改超时时间

--- src/main/java/pornhub/download/util/ProxyUtil.java
+++ src/main/java/pornhub/download/util/ProxyUtil.java
@@ -1,17 +1,23 @@
 package pornhub.download.util;
 
 import cn.hutool.http.HttpRequest;
+import cn.hutool.log.Log;
 import pornhub.download.Main;
 import pornhub.download.entity.Config;
 
 public class ProxyUtil {
+    private static final Log log = Log.get(ProxyUtil.class);
+
     public static HttpRequest addProxy(HttpRequest request) {
         request.setConnectionTimeout(3000)
                 .timeout(3000);
         Config config = Main.config;
         Config.Proxy proxy = config.getProxy();
         if (proxy.hasProxy()) {
+            log.info("proxy" + request.getUrl());
             request.setHttpProxy(proxy.getHost(), proxy.getPort());
+        }else {
+            log.info("not proxy" + request.getUrl());
         }
         return request;
     }
